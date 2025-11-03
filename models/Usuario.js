4247a42382f99b6ffe2bf434e1a4ab042024d0bd
EDUARDODLC19 <eduardodlcruz05@gmail.com>
2025-11-03 14:16:13 -0500

    Delete models directory

--- models/Usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const usuarioSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    correo: { type: String, required: true, unique: true },
-    edad: { type: Number }
-});
-
-module.exports = mongoose.model('Usuario', usuarioSchema);
